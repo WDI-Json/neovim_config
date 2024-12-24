37f601f10a79cffa847c7bae6a862e112fd14c24
Wouter Dijks <wouter.dijks@topicus.nl>
2024-12-24 11:45:52 +0100

    update config with none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,8 +6,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop
+				null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop
 			}
 		})
 		vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
