7c483e9f77bf97757584e92b44acee01153f5c34
Wouter Dijks <wouter.dijks@topicus.nl>
2025-01-08 15:57:20 +0100

    fixed the formatter in ruby

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,12 +4,19 @@ return {
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
+			--debug = true,
 			sources = {
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.rubocop
-			}
+			},
 		})
-		vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-	end
+		vim.keymap.set("n", "<leader>gf", function()
+			vim.lsp.buf.format({ async = true }) -- Use async to avoid blocking
+		end, {})
+
+    local logger = require("null-ls.logger")
+    logger.warn = function() end -- Suppress warnings
+	end,
 }
