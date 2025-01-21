3c59166972c956c3e5d23ba0b8742ab5d551a73e
Wouter Dijks <wouter.dijks@topicus.nl>
2025-01-21 11:00:32 +0100

    making hidden files visible

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,16 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, 
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
+
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,15 +8,29 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {},
+          hidden = true, 
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            follow = true,
+          },
+          live_grep = {
+            additional_args = function() return { "--hidden", "--no-ignore" } end, 
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
       })
+
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-p>", function() builtin.find_files({ hidden = true }) end, {}) 
+      vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ additional_args = { "--hidden", "--no-ignore" } }) end, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
       require("telescope").load_extension("ui-select")
