9948f4e83b452d9c44d2331733e52425ab02c713
Wouter Dijks <wouter.dijks@topicus.nl>
2025-01-22 15:06:54 +0100

    back to basics with settings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,17 +7,12 @@ return {
 			--debug = true,
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.erb_lint,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.diagnostics.haml_lint,
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", function()
-			vim.lsp.buf.format({ async = true }) -- Use async to avoid blocking
-		end, {})
-
-    local logger = require("null-ls.logger")
-    logger.warn = function() end -- Suppress warnings
+		
+    -- local logger = require("null-ls.logger")
+    -- logger.warn = function() end -- Suppress warnings
 	end,
 }
